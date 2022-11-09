15fb2c03e150cfd267fa12a1f173eb50ab336bc1
kangsk <kang991281@gmail.com>
2022-11-09 16:13:18 +0900

    Modify Weight and Add Minus/Divide Data and Weight

--- Assets/Scripts/Weight/RandomCard.cs
+++ Assets/Scripts/Weight/RandomCard.cs
@@ -7,8 +7,6 @@ public class RandomCard : MonoBehaviour
     public WeightedRandomList<GameObject> rewardList;
     public WeightedRandomList<GameObject> storeList;
     public WeightedRandomList<GameObject> artifactList;
-    public WeightedRandomList<GameObject> battleAttackList;
-    public WeightedRandomList<GameObject> battleHealList;
 
     public GameObject[] storeItems;
     public GameObject[] rewardItems;
@@ -22,6 +20,7 @@ public class RandomCard : MonoBehaviour
         }
         Debug.Log("Weight Insert Succeed");
     }
+
     public void ShowStoreItem()
     {
         for (int i = 0; i < 3; i++)
